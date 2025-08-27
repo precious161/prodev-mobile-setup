1d49a2fd8b64d64c09e81f60fbf8739be45c403c
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-08-27 16:22:30 +0300

    Fixed the errors

--- prodev-mobile-app-0X01/app/index.tsx
+++ prodev-mobile-app-0X01/app/index.tsx
@@ -1,42 +1,37 @@
-import { Text, View } from "react-native";
-import { StyleSheet } from "react-native"
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={
-        styles.container
-      }
-    >
+    <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
       <View>
-      <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-      <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-      <Text style={styles.smallText}>ALX is awesome</Text>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
   );
 }
 
-const styles= StyleSheet.create({
-  container:{
+const styles = StyleSheet.create({
+  container: {
     backgroundColor: "#90caf9",
   },
-  largeText:{
+  largeText: {
     fontSize: 30,
     color: "#f44336",
     marginBottom: 5,
     fontWeight: "700",
     fontVariant: ["small-caps"],
   },
-  mediumText:{
+  mediumText: {
     fontSize: 20,
     color: "#9c27b0",
     marginBottom: 10,
     fontWeight: "500",
     textAlign: "right",
   },
-  smallText:{
+  smallText: {
     fontSize: 15,
     color: "#2196f3",
     fontWeight: "400",
